fb97ddf7d393c97233ffbff425922c1eae7f8b99
madstone290 <madstone290@gmail.com>
2022-09-19 21:32:48 +0900

    Add 인덱스 페이지 추가
    사용하지 않는 템플릿 파일 삭제

---
+++ src/WebApp/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Blazor and Razor services
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// MudBlazor service
+builder.Services.AddMudServices(options =>
+{
+    options.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+    options.SnackbarConfiguration.PreventDuplicates = false;
+    options.SnackbarConfiguration.NewestOnTop = false;
+    options.SnackbarConfiguration.ShowCloseIcon = true;
+    options.SnackbarConfiguration.MaxDisplayedSnackbars = 10;
+    options.SnackbarConfiguration.VisibleStateDuration = 5000;
+    options.SnackbarConfiguration.ShowTransitionDuration = 0;
+    options.SnackbarConfiguration.HideTransitionDuration = 1000;
+
+    options.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
+builder.Services.AddSingleton<HttpClient>((sp) =>
+{
+    var apiAddress = builder.Environment.IsProduction()
+        ? new Uri(builder.Configuration["ApiUrls:Production"])
+        : new Uri(builder.Configuration["ApiUrls:Development"]);
+
+    return new HttpClient()
+    {
+        BaseAddress = apiAddress
+    };
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
