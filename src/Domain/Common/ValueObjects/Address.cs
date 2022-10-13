1c8f5454060aada96a737f93d2ae9c62e7ecc015
madstone290 <madstone290@gmail.com>
2022-10-13 14:00:41 +0900

    fix AddressVO 동일성 검사 항목 수정
    동일성 검사에 Details 속성 추가

--- src/Domain/Common/ValueObjects/Address.cs
+++ src/Domain/Common/ValueObjects/Address.cs
@@ -69,6 +69,7 @@ namespace Bridge.Domain.Common.ValueObjects
         {
             yield return RoadAddress;
             yield return JibunAddress;
+            yield return Details;
         }
 
     }
