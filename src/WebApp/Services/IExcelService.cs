3002cf633ccf10902e80f9dd3baec08d0b75349f
madstone290 <madstone290@gmail.com>
2022-10-26 12:59:17 +0900

    feat ExpandedXml dll 적용

--- src/WebApp/Services/IExcelService.cs
+++ src/WebApp/Services/IExcelService.cs
@@ -1,9 +1,10 @@
-using ClosedXML.Excel;
 using MudBlazor;
 using System.Reflection;
 using Bridge.WebApp.Shared;
-using Bridge.Shared;
-using DocumentFormat.OpenXml.Office2021.MipLabelMetaData;
+using ExpandedXml.Readers;
+using ExpandedXml.Readers.CellReaders.CellTextConverters;
+using ExpandedXml;
+using ExpandedXml.Writers;
 
 namespace Bridge.WebApp.Services
 {
@@ -16,9 +17,9 @@ namespace Bridge.WebApp.Services
         /// 엑셀 테이블을 읽고 T타입 인스턴스로 변환한다.
         /// </summary>
         /// <typeparam name="T">인스턴스 타입</typeparam>
-        /// <param name="stream">엑셀 데이터</param>
+        /// <param name="data">엑셀 데이터</param>
         /// <returns></returns>
-        IEnumerable<T> ReadTable<T>(Stream stream, ExcelOptions? options = null);
+        IEnumerable<T> ReadTable<T>(byte[] data, ExcelOptions? options = null);
 
         /// <summary>
         /// 엑셀 테이블을 생성한다.
@@ -26,7 +27,7 @@ namespace Bridge.WebApp.Services
         /// <typeparam name="T">인스턴스 타입</typeparam>
         /// <param name="list">인스턴스 리스트</param>
         /// <returns></returns>
-        Stream WriteTable<T>(IEnumerable<T> list, ExcelOptions? options = null);
+        byte[] WriteTable<T>(IEnumerable<T> list, ExcelOptions? options = null);
 
         /// <summary>
         /// 엑셀파일을 업로드한다.
@@ -74,162 +75,42 @@ namespace Bridge.WebApp.Services
     {
         private readonly IDialogService _dialogService;
         private readonly IFileService _fileService;
+        private readonly IExReader _exReader = new ExReader();
+        private readonly IExWriter _exWriter = new ExWriter();
 
         public ExcelService(IDialogService dialogService, IFileService fileService)
         {
             _dialogService = dialogService;
             _fileService = fileService;
+
+            var boolTextConvertet = new BoolTextConverter();
+            boolTextConvertet.AddTrueString("y");
+            boolTextConvertet.AddTrueString("Y");
+            boolTextConvertet.AddTrueString("1");
+            _exReader.AddCellTextConverter(typeof(bool), boolTextConvertet);
         }
 
-        /// <summary>
-        /// .net 타입에 대응하는 XLDataTyp을 반환한다.
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        private static XLDataType GetXLDataType(Type type)
+        private static IEnumerable<ExColumnHeader> CreateHeaders(IEnumerable<PropertyInfo> propertyInfos, IEnumerable<ExcelOptions.Column>? optionsColumns)
         {
-            if (type == typeof(TimeSpan?) || type == typeof(TimeSpan))
+            return propertyInfos.Select(pInfo =>
             {
-                return XLDataType.TimeSpan;
-            }
-            else if (type == typeof(DateTime?) || type == typeof(DateTime))
-            {
-                return XLDataType.DateTime;
-            }
-            else if (type == typeof(bool?) || type == typeof(bool))
-            {
-                return XLDataType.Boolean;
-            }
-            else if (type == typeof(int?) || type == typeof(int) ||
-                type == typeof(long?) || type == typeof(long) ||
-                type == typeof(double?) || type == typeof(double) ||
-                type == typeof(float?) || type == typeof(float) ||
-                type == typeof(decimal?) || type == typeof(decimal) ||
-                type == typeof(short?) || type == typeof(short))
-            {
-                return XLDataType.Number;
-            }
-            else
-            {
-                return XLDataType.Text;
-            }
+                var caption = optionsColumns?.FirstOrDefault(c => string.Equals(c.Name, pInfo.Name))?.Caption ?? pInfo.Name;
+                var propertyType = Nullable.GetUnderlyingType(pInfo.PropertyType) ?? pInfo.PropertyType;
+                return new ExColumnHeader(pInfo.Name, caption, propertyType);
+            });
         }
 
-        public IEnumerable<T> ReadTable<T>(Stream stream, ExcelOptions? options = null)
+        public IEnumerable<T> ReadTable<T>(byte[] data, ExcelOptions? options = null)
         {
-            var list = new List<T>();
-            var workBook = new XLWorkbook(stream);
-            var worksheet = workBook.Worksheet(1);
-
-            var sheetRows = worksheet.RowsUsed();
-            var sheetColumns = worksheet.ColumnsUsed();
-            if (!sheetRows.Any() || !sheetColumns.Any())
-                return list;
-
             var properties = typeof(T).GetProperties().Where(x => x.CanWrite);
-            var optionsColumns = options?.Columns ?? properties.Select(prop => new ExcelOptions.Column(prop.Name, prop.Name)).ToList();
-
-            // 컬럼 캡션(헤더텍스트), 컬럼번호(데이터 읽기), 컬럼명(인스턴스 속성)
-            // 컬럼번호/인스턴스속성 사전
-            var columnNumberProperties = new Dictionary<int, PropertyInfo>();
-
-            // read header row
-            var headerRow = sheetRows.First();
-            foreach (var column in sheetColumns)
-            {
-                var columnNumber = column.ColumnNumber();
-                var headerCell = headerRow.Cell(columnNumber);
-                var columnCaption = headerCell.GetString();
-                var optionsColumn = optionsColumns.FirstOrDefault(x => string.Equals(x.Caption, columnCaption, StringComparison.OrdinalIgnoreCase));
-                if (optionsColumn == null)
-                    continue;
-
-                var property = properties.FirstOrDefault(x => string.Equals(x.Name, optionsColumn.Name, StringComparison.OrdinalIgnoreCase));
-                if (property == null)
-                    continue;
-
-                // 컬럼번호에 해당하는 속성을 추가한다
-                columnNumberProperties.Add(columnNumber, property);
-
-                // .Net타입에 맞는 XLDataType을 설정한다
-                XLDataType dataType = GetXLDataType(property.PropertyType);
-                worksheet.Range(2, columnNumber, sheetRows.Count(), columnNumber).DataType = dataType;
-            }
-
-            // read data rows
-            foreach (var dataRow in sheetRows.Skip(1))
-            {
-                var instance = (T)Activator.CreateInstance(typeof(T))!;
-                foreach (var column in sheetColumns)
-                {
-                    var columnNumber = column.ColumnNumber();
-                    if (!columnNumberProperties.ContainsKey(columnNumber))
-                        continue;
-                    try
-                    {
-                        var property = columnNumberProperties[columnNumber];
-                        var cell = dataRow.Cell(columnNumber);
-                        var cellValue = cell.Value;
-
-                        var propertyValue = ObjectConverter.Default.Execute(property.PropertyType, cellValue);
-
-                        property.SetValue(instance, propertyValue);
-                    }
-                    catch { } // Failed to change value type 
-                }
-                list.Add(instance);
-            }
-
-            return list;
+            return _exReader.Read<T>(data, CreateHeaders(properties, options?.Columns));
         }
 
-        public Stream WriteTable<T>(IEnumerable<T> list, ExcelOptions? options = null)
+        public byte[] WriteTable<T>(IEnumerable<T> list, ExcelOptions? options = null)
         {
-            var wbook = new XLWorkbook();
-            var worksheet = wbook.AddWorksheet();
-
             var properties = typeof(T).GetProperties().Where(x => x.CanWrite);
-            var optionsColumns = options?.Columns ?? properties.Select(prop => new ExcelOptions.Column(prop.Name, prop.Name)).ToList();
-            var columnNumberProperties = new Dictionary<int, PropertyInfo>();
-
-            var rowNumber = 1;
-            var columnNumber = 1;
-            // write header row
-            foreach (var optionsColumn in optionsColumns)
-            {
-                worksheet.Cell(rowNumber, columnNumber).Value = optionsColumn.Caption;
-
-                var property = properties.FirstOrDefault(x => string.Equals(x.Name, optionsColumn.Name, StringComparison.OrdinalIgnoreCase));
-                if (property != null)
-                {
-                    columnNumberProperties.Add(columnNumber, property);
-                }
-                columnNumber++;
-            }
-            rowNumber++;
-
-            // write data rows
-            foreach (var instance in list)
-            {
-                columnNumber = 1;
-                foreach (var column in optionsColumns)
-                {
-                    if (columnNumberProperties.ContainsKey(columnNumber))
-                    {
-                        var property = columnNumberProperties[columnNumber];
-                        var cell = worksheet.Cell(rowNumber, columnNumber);
-                        var value = property.GetValue(instance);
-
-                        cell.SetValue(value);
-                    }
-                    columnNumber++;
-                }
-                rowNumber++;
-            }
-            var stream = new MemoryStream();
-            wbook.SaveAs(stream);
-
-            return stream;
+            var data = _exWriter.Write<T>(list, CreateHeaders(properties, options?.Columns));
+            return data;
         }
 
         public async Task<IEnumerable<T>> UploadAsync<T>(ExcelOptions? options = null)
@@ -248,8 +129,7 @@ namespace Bridge.WebApp.Services
 
             if (!result.Cancelled)
             {
-                using var stream = (Stream)result.Data;
-                return ReadTable<T>(stream, options);
+                return ReadTable<T>((byte[])result.Data, options);
             }
             else
             {
@@ -259,8 +139,9 @@ namespace Bridge.WebApp.Services
 
         public async Task DownloadAsync<T>(string fileName, IEnumerable<T> list, ExcelOptions? options = null)
         {
-            using var stream = WriteTable(list, options);
-            await _fileService.DownloadFileAsync(fileName, stream);
+            var data = WriteTable(list, options);
+            using var memoryStream = new MemoryStream(data);
+            await _fileService.DownloadFileAsync(fileName, memoryStream);
         }
     }
 
